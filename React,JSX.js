da31006b1b24fae0748ef73e9399abab8782b9c5
dzh0041 <54254184+dzh0041@users.noreply.github.com>
2020-10-19 12:45:58 +0900

    Update React,JSX.js

--- React,JSX.js
+++ React,JSX.js
@@ -7,11 +7,11 @@ class QuoteMaker extends React.Component {
   render() {
     return (
       <blockquote>
-  <p>
-    What is important now is to recover our senses.
-  </p>
-  <cite>
-    <a target="_blank" 
+        <p>
+          What is important now is to recover our senses.
+        </p>
+        <cite>
+          <a target="_blank" 
       href="https://en.wikipedia.org/wiki/Susan_Sontag">
       Susan Sontag
     </a>
