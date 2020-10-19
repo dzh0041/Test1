4c998cd1a6509ccb76aae7c0ab9082baa8eb2bb3
dzh0041 <54254184+dzh0041@users.noreply.github.com>
2020-10-19 13:37:14 +0900

    Update React,JSX.js

--- React,JSX.js
+++ React,JSX.js
@@ -59,3 +59,26 @@ class Friend extends React.Component {
 }
 
 ReactDOM.render(<Friend />, document.getElementById('app'));
+
+//if 문을 사용하여 상태에 따라 나오는 답변이 달라짐
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const fiftyFifty = Math.random() < 0.5;
+
+// New component class starts here:
+class TonightsPlan extends React.Component {
+  render() {
+    if (fiftyFifty) {
+      return <h1>Tonight I'm going out WOOO</h1>;
+    } else {
+      return <h1>Tonight I'm going to bed WOOO</h1>;
+    }
+  }
+}
+
+ReactDOM.render(
+	<TonightsPlan />,
+	document.getElementById('app')
+);
